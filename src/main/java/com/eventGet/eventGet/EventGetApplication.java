334ae9b24f1e5f675e6bd848b3096e5a843b6206
kevin <kevinzamora239@gmail.com>
2021-03-08 16:56:34 -0300

    comiezo

---
+++ src/main/java/com/eventGet/eventGet/EventGetApplication.java
@@ -0,0 +1,13 @@
+package com.eventGet.eventGet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventGetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventGet/eventGet/EventGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventGet.eventGet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
